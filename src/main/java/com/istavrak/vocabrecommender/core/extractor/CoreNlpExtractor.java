c68f0cb3983fc8a15f722744c4326c224cc683ff
Ioannis Stavrakantonakis <gstavrak@gmail.com>
2017-02-13 22:37:23 +0100

    Fix dependencies for NLP

--- src/main/java/com/istavrak/vocabrecommender/core/extractor/CoreNlpExtractor.java
+++ src/main/java/com/istavrak/vocabrecommender/core/extractor/CoreNlpExtractor.java
@@ -2,7 +2,6 @@ package com.istavrak.vocabrecommender.core.extractor;
 
 
 import com.istavrak.vocabrecommender.model.TargetPage;
-import edu.stanford.nlp.simple.*;
 
 import java.util.*;
 
@@ -11,17 +10,7 @@ public class CoreNlpExtractor implements TokensExtractor {
     private final String NOUN_POS = "NN";
 
     private List<String> extractKeywords(String content) {
-        Document doc = new Document(content);
-        Set<String> tokens = new HashSet<>();
-        for (Sentence sent : doc.sentences()) {
-            List<String> words = sent.words();
-            for (int i = 0; i < words.size(); i++) {
-                if (words.get(i).length() > 2 && NOUN_POS.equals(sent.posTag(i))) {
-                    tokens.add(words.get(i).toLowerCase());
-                }
-            }
-        }
-        return !tokens.isEmpty() ? new ArrayList<>(tokens) : Collections.<String>emptyList();
+       return null;
     }
 
     @Override
